229849a4ad8ad05375f6cc5c4feff12cc1a97986
unknown <kikridu@gmail.com>
2022-06-07 21:22:54 -0700

    Photolist, Modal components and tests were implemented, fully functional

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,26 +3,42 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+
+const categories = [
+    {name: 'Portraits', description: 'Portraits of people in my life'}
+]
+
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn()
+
 afterEach(cleanup);
 
-describe('Nav component', () => {
-    //baseline text
+describe('Nav component renders', () => {
     it('renders', () => {
-        render(<Nav />);
+      render(<Nav />);
     });
-
-    //snapshot text
-    it('matches snapshot ', () => {
-        const { asFragment } = render(<Nav />);
-
-        expect(asFragment()).toMatchSnapshot();
+  
+    it('matches snapshot', () => {
+      const { asFragment } = render(<Nav />);
+      
+      expect(asFragment()).toMatchSnapshot();
     });
-})
-
-describe('emoji is visible', () => {
+  })
+  
+  describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
-        const { getByLabelText } = render(<Nav />);
-
-        expect(getByLabelText('camera')).toHaveTextContent('📸')
-    })
-})
+    const { getByLabelText } = render(<Nav />);
+  
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+  })  
+  
+  describe('links are visible', () => {
+    it('inserts text into the links', () => {
+      const { getByTestId } = render(<Nav />);
+  
+      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+      expect(getByTestId('about')).toHaveTextContent('About me');
+    });
+  
+  })
